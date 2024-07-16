1a9dddb073087d7e6e04f83a51711eae8459e393
chrismrkr <b635032_@daum.net>
2024-07-16 11:56:43 +0900

    UPD: heartbeat and reconnect

--- src/privateroom/PrivateChatRoom.js
+++ src/privateroom/PrivateChatRoom.js
@@ -9,30 +9,55 @@ const PrivateChatRoom = () => {
     const {roomId} = useParams();
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
+
     const client = useRef({});
+    const [isConnected, setIsConnected] = useState(false);
+    const [tryReconnect, setTryReconnect] = useState(true);
     const connect = () => {
+        if(isConnected) {
+            return;
+        }
+        console.log(`WEBSOCKET EVENT: OPEN`)
         client.current = new Client({
             // brokerURL: 'ws://localhost:8080/ws', // local
             brokerURL: 'ws://localhost:80/ws',
-            onConnect: () => {
-                console.log("connected!");
-                client.current.subscribe(`/exchange/chat.exchange/roomId.${roomId}`, (msg) => {
-                // client.current.subscribe(`/chatroom/${roomId}`, (msg) => {
-                    const resBody = JSON.parse(msg.body);
-                    console.log(msg.body)
-                    setMessages((prevMessages) => [...prevMessages, resBody]);
-                })
-            }
+            heartbeatIncoming: 20000, // server -> client heartbeat 20s
+            heartbeatOutgoing: 20000, // client -> server heartbeat 20s
         });
+        client.current.onConnect = () => { // websocket 연결 성공 콜백
+            setIsConnected(true);
+            client.current.subscribe(`/exchange/chat.exchange/roomId.${roomId}`, (msg) => {
+            // client.current.subscribe(`/chatroom/${roomId}`, (msg) => {
+                const resBody = JSON.parse(msg.body);
+                setMessages((prevMessages) => [...prevMessages, resBody]);
+            })
+            console.log("WEBSOCKET EVENT: CONNECT")
+        };
+        client.current.onWebSocketClose = (event) => { // websocket 연결 해제 콜백
+            setIsConnected(false);
+            console.log(`WEBSOCKET EVENT: CLOSE ${event.code} ${event.reason}`);
+            if(tryReconnect) {
+                setTimeout(() => {
+                    connect();
+                }, 3000);
+            }
+        };
         client.current.activate();
     };
+
     const disconnect = () => {
-        client.current.deactivate();
+        console.log(`WEBSOCKET EVENT: DISCONNECT`)
+        setTryReconnect(false);
+        client.current.deactivate(() => {
+            setIsConnected(false);
+        });
     }
+
     useEffect(() => {
         connect();
         return () => disconnect();
     }, []);
+
     const publishMessage = (event) => {
         event.preventDefault();
         if(!client.current.connected) return;
